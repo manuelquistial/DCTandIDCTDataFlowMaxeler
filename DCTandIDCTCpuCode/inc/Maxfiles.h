a33f60df4058274be17cfa0347133393b0cb6e62
maxeler <maxeler@MaxCompilerVMTemplate.myguest.virtualbox.org>
2019-03-06 04:06:13 +0000

    Discrete Cosine Transform and their inverse with Dataflow Computing in Maxeler

---
+++ DCTandIDCTCpuCode/inc/Maxfiles.h
@@ -0,0 +1,11 @@
+/* File Maxfiles.h  */
+#ifndef MAXFILES_H
+#define MAXFILES_H
+
+/**
+ * Add .h files here or use MaxFileManager Dialog, 
+ * to set up which Maxfiles headers 
+ * are to be used with this project.
+ */
+
+#endif /* !MAXFILES_H */
